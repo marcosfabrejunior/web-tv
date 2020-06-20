deebc267bff1568e6e188ef3e60c5ed44512f3f1
marcosfabrejr <marcosfabrejunior@hotmail.com>
2020-06-20 11:36:59 -0300

    automatizando inicio

--- src/assets/channel.ts
+++ src/assets/channel.ts
@@ -1,3 +1,10 @@
+let date = new Date();
+let date_init = date.getTime() - 60*1000;
+let date_end = date.getTime() + 20*60*1000;
+let date_init_string = new Date(date_init).toString();
+let date_end_string =  new Date(date_end).toString();
+
+console.log(date_init_string);
 
 export default
     [
@@ -10,10 +17,10 @@ export default
                 "name": "Os predadores da África em ação | África Selvagem: Rios | Animal Planet Brasil",
                 "description": "Neste super documentário, conheça como vivem os predadores na África Selvagem.",
                 "videoId": "qPEYfRXg5ws",
-                "start": "2020-04-06T22:22:00",
-                "end": "2020-04-06T23:00:00"
+                "start": date_init_string,
+                "end": date_end_string
             },
-            "nextChannel":  {
+            "nextChannel":  { 
                 "id": 10,
                 "name": "TechPlay",
                 "description": "Sempre Atualizado com as novidades no mundo da Tecnologia",
@@ -22,8 +29,8 @@ export default
                     "name": "Evan You - State of Vue",
                     "description": "Palestra do criador de um dos maiores frameworks Javascript na VueConf 2017",
                     "videoId": "lG0Ys-2d4MA",
-                    "start": "2020-04-06T22:22:00",
-                    "end": "2020-04-06T23:00:00"
+                    "start": date_init_string,
+                    "end": date_end_string
                 },
                 "nextChannel": {
                     "id": 11,
@@ -34,8 +41,8 @@ export default
                         "name": "Os predadores da África em ação | África Selvagem: Rios | Animal Planet Brasil",
                         "description": "Neste super documentário, conheça como vivem os predadores na África Selvagem.",
                         "videoId": "qPEYfRXg5ws",
-                        "start": "2020-04-06T22:22:00",
-                        "end": "2020-04-06T23:00:00"
+                        "start": date_init_string,
+                        "end": date_end_string
                     },
                     "nextChannel": 11,
                     "previousChannel": 11
@@ -49,8 +56,8 @@ export default
                         "name": "Os predadores da África em ação | África Selvagem: Rios | Animal Planet Brasil",
                         "description": "Neste super documentário, conheça como vivem os predadores na África Selvagem.",
                         "videoId": "qPEYfRXg5ws",
-                        "start": "2020-04-06T22:22:00",
-                        "end": "2020-04-06T23:00:00"
+                        "start": date_init_string,
+                        "end": date_end_string
                     },
                     "nextChannel": 11,
                     "previousChannel": 11
@@ -65,8 +72,8 @@ export default
                     "name": "Evan You - State of Vue",
                     "description": "Palestra do criador de um dos maiores frameworks Javascript na VueConf 2017",
                     "videoId": "lG0Ys-2d4MA",
-                    "start": "2020-04-06T22:22:00",
-                    "end": "2020-04-06T23:00:00"
+                    "start": date_init_string,
+                    "end": date_end_string
                 },
                 "nextChannel": {
                     "id": 11,
@@ -77,8 +84,8 @@ export default
                         "name": "Os predadores da África em ação | África Selvagem: Rios | Animal Planet Brasil",
                         "description": "Neste super documentário, conheça como vivem os predadores na África Selvagem.",
                         "videoId": "qPEYfRXg5ws",
-                        "start": "2020-04-06T22:22:00",
-                        "end": "2020-04-06T23:00:00"
+                        "start": date_init_string,
+                        "end": date_end_string
                     },
                     "nextChannel": 11,
                     "previousChannel": 11
@@ -92,8 +99,8 @@ export default
                         "name": "Os predadores da África em ação | África Selvagem: Rios | Animal Planet Brasil",
                         "description": "Neste super documentário, conheça como vivem os predadores na África Selvagem.",
                         "videoId": "qPEYfRXg5ws",
-                        "start": "2020-04-06T22:22:00",
-                        "end": "2020-04-06T23:00:00"
+                        "start": date_init_string,
+                        "end": date_end_string
                     },
                     "nextChannel": 11,
                     "previousChannel": 11
